09a599acbcaf5ac50353ec15ecdae17b8cd9b370
wadudrahman <wadud1234@gmail.com>
2025-10-06 22:21:42 +0600

    Defines API routes; Created Controllers

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,26 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::group(['prefix' => 'v1'], function () {
+    // Authentication Routes
+    Route::group(['prefix' => 'auth'], function () {
+        Route::post('/login', [AuthController::class, 'login']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/whoami', [AuthController::class, 'me'])->middleware('auth:sanctum');
+    });
+
+    // Transactions Routes
+//    Route::middleware('auth:sanctum')->group(['prefix' => 'transactions'], function () {
+//        Route::get('/', [TransactionController::class, 'getTransactions']);
+//        Route::post('/', [TransactionController::class, 'storeTransaction']);
+//    });
+
+});
